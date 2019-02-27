e57bbc75e9b42ad3137c97a2cca0f1c2d952d8e6
Liridon Smajli <vondony@hotmail.com>
2019-02-27 16:40:37 +0100

    LoginActivity added, removed MainActivity Database in progress, started

--- app/src/main/java/com/example/androidchess/Database.java
+++ app/src/main/java/com/example/androidchess/Database.java
@@ -1,9 +1,6 @@
 package com.example.androidchess;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Database {
 
@@ -13,8 +10,8 @@ public class Database {
     public Database(){
 
         try {
-            String url = "jdbc:mysql://localhost/shack";
-            connect = (Connection) DriverManager.getConnection(url);
+            String url = "jdbc:mysql://localhost";
+            connect = DriverManager.getConnection(url);
             System.out.println("Connected to database");
         } catch (SQLException ex) {
             System.out.println("Failed to connect to database");
@@ -22,10 +19,10 @@ public class Database {
 
     }
 
-    //For registering accounts
-    public void addUser( String username,String email, String password, int account_id) {
+    //For registering user
+    public void registerUser( String username,String email, String password, int account_id) {
 
-        String query = "insert into user (username, email, password, id)" + "values (?, ?, ?, ?)";
+        String query = "INSERT INTO shack.user (username, email, password, id)" + "VALUES (?, ?, ?, ?)";
         // create the mysql insert preparedStatement
         try(PreparedStatement preparedStmt = connect.prepareStatement(query)) {
             preparedStmt.setString(1, username);
@@ -38,6 +35,30 @@ public class Database {
         }catch (Exception e){
             e.printStackTrace();
         }
+    }
+
+    //For authentication of logging in into the app as a user
+    public boolean authenticateUser(String username, String password){
+        boolean flag = false;
+        String query = "SELECT username, password FROM shack.user where username = ? AND password = ?";
+        try(PreparedStatement preparedStmt = connect.prepareStatement(query)) {
+            preparedStmt.setString(1, username);
+            preparedStmt.setString(2, password);
+
+            ResultSet resultSet = preparedStmt.executeQuery();
+            resultSet.next();
+            String username1 = resultSet.getString("username");
+            String password1 = resultSet.getString("password");
+
+            if (username1.equals(username) && password1.equals(password)){
+                flag = true;
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return flag;
     }
 
 
