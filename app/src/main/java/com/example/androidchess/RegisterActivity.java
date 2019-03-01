a980274a798f4facca05a65972affa44f1dbbc79
Liridon Smajli <vondony@hotmail.com>
2019-03-01 15:08:17 +0100

    LogIn updated, Register in Progress

---
+++ app/src/main/java/com/example/androidchess/RegisterActivity.java
@@ -0,0 +1,31 @@
+package com.example.androidchess;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private Button mReturnButton;
+    private Button mRegisterButton;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mReturnButton = findViewById(R.id.return_button);
+        mRegisterButton = findViewById(R.id.register_button);
+
+        mReturnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent returnActivity = new Intent(RegisterActivity.this, LogInActivity.class);
+                startActivity(returnActivity);
+            }
+        });
+
+    }
+
+}
