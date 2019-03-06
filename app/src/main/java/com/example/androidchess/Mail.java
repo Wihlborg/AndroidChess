572f13a97fe3814ed5b9db4da7372614582cfbd0
Liridon Smajli <vondony@hotmail.com>
2019-03-06 20:57:53 +0100

    Added mail functionality for password recovery

---
+++ app/src/main/java/com/example/androidchess/Mail.java
@@ -0,0 +1,63 @@
+package com.example.androidchess;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class Mail {
+    private static Mail mailObject = null;
+    private String host = "smtp.gmail.com";
+    private String port = "587";
+    private final String shackUser = "android.tjack@gmail.com";
+    private final String shackPassword = "password";
+    private boolean authentication=true;
+    private boolean smtpServerTTLSEnabled = true;
+    private Session session;
+
+
+    //Instance
+    static Mail getInstance(){
+        if (mailObject == null){
+            mailObject = new Mail();
+        }
+        return mailObject;
+    }
+
+    //Mail constructor
+    private Mail(){
+        Properties props = new Properties();
+        props.put("mail.smtp.auth", String.valueOf(authentication));
+        props.put("mail.smtp.starttls.enable",smtpServerTTLSEnabled);
+        props.put("mail.smtp.host", host);
+        props.put("mail.smtp.port", port);
+        session = Session.getInstance(props, new Authenticator() {
+            @Override
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(shackUser, shackPassword);
+            }
+        });
+    }
+
+    public void sendEmail(String recipientEmail, String recoverPassword, String username){
+
+        try {
+
+            Message message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(shackUser));
+            message.setRecipients(Message.RecipientType.TO,
+                    InternetAddress.parse(recipientEmail));
+            message.setSubject("Password Recovery");
+            message.setText("Hello, " + username + "\nYour new password: " + recoverPassword
+            + "\n Regards, " + "\nAndroidChess");
+
+            Transport.send(message);
+
+            System.out.println("Done");
+
+        } catch (MessagingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
