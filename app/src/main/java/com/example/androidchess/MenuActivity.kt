d2f41eedb137f11f86961ca1a4792e52f95a3eee
Wihlborg <henrik.vihlborg0017@stud.hkr.se>
2019-03-08 02:29:11 +0100

    Added empty functions for activity changes

--- app/src/main/java/com/example/androidchess/MenuActivity.kt
+++ app/src/main/java/com/example/androidchess/MenuActivity.kt
@@ -2,12 +2,42 @@ package com.example.androidchess
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.Button
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
         supportActionBar!!.hide()
+
+        val multiPlayerButton = findViewById<Button>(R.id.multiplayerbutton)
+        multiPlayerButton.setOnClickListener { playMultiplayer() }
+
+        val aiButton = findViewById<Button>(R.id.aibutton)
+        aiButton.setOnClickListener { playAI() }
+
+        val optionsButton = findViewById<Button>(R.id.optionbutton)
+        optionsButton.setOnClickListener{ options() }
+
+        val logOutButton = findViewById<Button>(R.id.logoutbutton)
+        logOutButton.setOnClickListener { logOut() }
+    }
+
+    //TODO: implement intents for changing activity
+    fun playMultiplayer(){
+
+    }
+
+    fun playAI(){
+
+    }
+
+    fun options(){
+
+    }
+
+    fun logOut(){
+
     }
 }
 
