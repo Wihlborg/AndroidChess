3b65fea12756264b907c0c5ffcb7f8c82b0e8e26
Wihlborg <henrik.vihlborg0017@stud.hkr.se>
2019-03-20 13:14:42 +0100

    Added sounds and option activity

---
+++ app/src/main/java/com/example/androidchess/Activities/OptionActivity.kt
@@ -0,0 +1,15 @@
+package com.example.androidchess.Activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.androidchess.*
+
+class OptionActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_option)
+    }
+
+
+}
