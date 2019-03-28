ec0e5d61f133a3d752a3ec13ef793b8ff318b21c
Ralle <r.olsson96@gmail.com>
2019-03-28 11:51:25 +0100

    last min

--- app/src/main/java/com/example/androidchess/Activities/LobbyActivity.java
+++ app/src/main/java/com/example/androidchess/Activities/LobbyActivity.java
@@ -52,6 +52,7 @@ public class LobbyActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
+        getSupportActionBar().hide();
         btnOnOFF = (Button) findViewById(R.id.onOff);
         btnDiscover = (Button) findViewById(R.id.discover);
         btnSend = (Button) findViewById(R.id.sendButton);
