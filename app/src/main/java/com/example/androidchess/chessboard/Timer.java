e13283409ee9be27e98fafc201d3b2fe4e3e08ab
Ralle <r.olsson96@gmail.com>
2019-05-14 16:20:21 +0200

    added timer class

---
+++ app/src/main/java/com/example/androidchess/chessboard/Timer.java
@@ -0,0 +1,26 @@
+package com.example.androidchess.chessboard;
+
+public class Timer {
+    private long startTime;
+    private long stopTime;
+
+    public void startTimer() {
+        startTime = 0;
+        startTime = System.nanoTime();
+        stopTime = 0;
+    }
+
+    public float stopTimer() {
+        stopTime = System.nanoTime();
+        return retreiveTime();
+    }
+
+    public float retreiveTime() {
+        return (float)(stopTime-startTime)/1000000000;
+    }
+
+    public void printTime() {
+        System.out.println((float)(stopTime-startTime)/1000000000);
+    }
+
+}
